d1140c0e774c76541ba0d2fce2addbaf28518776
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-12-10 22:34:02 +0100

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,18 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
 
         type: "asset/resource",
       },
@@ -36,6 +48,14 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
 
         type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
       },
     ],
   },
