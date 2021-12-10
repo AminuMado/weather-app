d96abd24662de35fb61ab8b3e72b9b0b654b8a08
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-12-10 12:32:10 +0100

    Got the weather object from open weather

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./style.css";
-const a = "ls";
+const apiKey = "135c67825987bafb2b6286a00bc0ce1f";
+let cityName = "Abuja";
+
+let weatherReport = fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`,
+  { mode: "cors" }
+);
+
+weatherReport
+  .then((response) => {
+    let report = response.json();
+    return report;
+  })
+  .then((response) => {
+    console.log(response);
+  });
+
+// console.log(weatherReport);
